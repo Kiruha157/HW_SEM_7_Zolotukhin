bf9fc81be5885d1c323cab43c7c35a34bc22701f
Kirill <kz@selfiemosaic.ru>
2024-02-07 11:27:35 +0300

    Task_1 comlete

---
+++ TASK_1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+// Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы.
+
+void ShowNumbers (int min, int max)
+{
+    if (min == max) 
+    {
+        Console.Write(min);
+        return;
+    }
+    Console.Write(min + " ");
+    ShowNumbers (min + 1, max);
+}
+
+ShowNumbers(5,10);
