64c6f7a73c918873fc620823921e0e28432e8f9d
Kirill <kz@selfiemosaic.ru>
2024-02-07 12:57:03 +0300

    TASK_3 complete

---
+++ TASK_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+int[] array = {1, 2, 3, 4, 5};
+int lengthArr = array.Length - 1;
+
+void ShowArrReverse(int[] arr, int len)
+{
+    int i = len;
+    if(len == 0)
+    {
+        Console.Write(arr[i]);
+        return;
+    }
+    Console.Write(arr[i] + " ");
+    ShowArrReverse(arr, i - 1);
+}
+
+Console.WriteLine($"Вывод элементов массива [{string.Join(" ", array)}]: ");
+
+ShowArrReverse(array, lengthArr);
+
+Console.WriteLine();
