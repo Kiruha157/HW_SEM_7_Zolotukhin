70f96bc79ff80e22311659a76b5c8cf96a6e0f5a
Kirill <kz@selfiemosaic.ru>
2024-02-07 12:16:01 +0300

    TASK_2 compete

---
+++ TASK_2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int CalcAck (int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    if (numN == 0)
+    {
+        return CalcAck (numM - 1, 1);
+    }
+    int result = CalcAck (numM - 1, CalcAck (numM, numN - 1));
+    return result;
+}
+
+Console.WriteLine($"Результат вычесления функции Аккермана: {CalcAck(3,10)}");
+
+// Понять бы зачем она нужна на практике...
